735f9b8ef19596df7d63d2b6fcc8cf15f3abdb9b
lee <lee199402@naver.com>
2017-01-15 02:10:35 +0900

    v1

--- sss/app/src/main/java/xxx/sss/LoginActivity.java
+++ sss/app/src/main/java/xxx/sss/LoginActivity.java
@@ -9,11 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private  AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +53,24 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
 
-                        try {
+                        try{
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
                             if(success){
-                                AlertDialog.Builder builder = new AlertDialog().Builder(LoginActivity.this);
-                                dialog =
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("로그인 성공")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                                finish();
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("로그인 실패")
+                                        .setNegativeButton("다시 시도", null)
+                                        .create();
+                                dialog.show();
 
                             }
                         }
@@ -64,7 +80,21 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     }
                 };
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseLister);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
+    }
+
+
+    @Override
+    protected  void onStop(){
+        super.onStop();
+        if(dialog != null)
+        {
+            dialog.dismiss();
+            dialog = null;
+        }
     }
 }
